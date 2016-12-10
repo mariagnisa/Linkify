fe75998293c103b58f2d96adf0b637e17d9b4d4e
Maria <maria.gnisa@gmail.com>
2016-12-10 15:57:14 +0100

    small fixes

--- login.php
+++ login.php
@@ -5,9 +5,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   //check if the input fields are not empty
   if ($_POST['username'] !== "" && $_POST['password'] !== "") {
 
-    if ($uid = LoginUser($db, $_POST['username'], $_POST['password'])) {
+    if ($uid = loginUser($db, $_POST['username'], $_POST['password'])) {
       session_start();
-      $_SESSION['login'] = [
+      //store the logged in users uid
+      $_SESSION['loginUser'] = [
       'uid' => $uid
       ];
     }
@@ -16,7 +17,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     session_start();
     $_SESSION['error'] = 'Please fill out all fields.';
   }
-  header('Location: views/home.php');
+
+  header('Location: /');
   die();
 }
 
