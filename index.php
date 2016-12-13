b82c9ba2296186515810464082333a41bb9584f0
Maria <maria.gnisa@gmail.com>
2016-12-13 12:21:17 +0100

    changed require, added alertbox for cookie

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require 'functions.php';
+require_once __DIR__.'/lib/functions.php';
 
 $loggedInUser = checkUserLogin($db);
  ?>
@@ -8,18 +8,22 @@ $loggedInUser = checkUserLogin($db);
   <head>
     <meta charset="utf-8">
     <title>Linkify</title>
+    <script src="/assets/js/main.js"></script>
   </head>
   <body>
-  <p>Hej </p>
   <?php
     //checks if a user has a active session
     if ($loggedInUser) {
       //if active session, the user will come to home page
-      require('views/home.php');
+      require_once __DIR__.'/views/home.php';
     } else {
       //otherwise user will come to authenication page for login/register
-      require('authenication.php');
+      require_once __DIR__.'/views/authenication.php';
     }
    ?>
+   <div class="alertbox">
+    <p>This website uses <a href="https://www.cookielaw.org/the-cookie-law">cookies</a> to ensure you get the best experience on our website.</p>
+  <button class="button" type="submit" name="button">Accept</button>
+  </div>
   </body>
 </html>
