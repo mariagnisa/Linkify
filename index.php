7ded66613b71fe625d0afc84643e6ca9b97fe818
Maria <maria.gnisa@gmail.com>
2016-12-14 19:58:04 +0100

    added title

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $loggedInUser = checkUserLogin($db);
 <html>
   <head>
     <?php
+    $title = 'Linkify';
     require_once __DIR__.'/views/head.php';
      ?>
   </head>
@@ -24,7 +25,7 @@ $loggedInUser = checkUserLogin($db);
    ?>
    <div class="cookie-box">
     <p>This website uses <a href="https://www.cookielaw.org/the-cookie-law">cookies</a> to ensure you get the best experience on our website.</p>
-  <button class="cookie-box__button" type="submit" name="button">Accept</button>
+  <button class="cookie-box__button" type="button" name="button">Accept</button>
   </div>
   </body>
 </html>
