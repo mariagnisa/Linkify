ce99f8e0d6c6db7e7c2141ff7ef03b458658edcb
Maria <maria.gnisa@gmail.com>
2016-12-14 11:51:58 +0100

    added files

--- views/header.php
+++ views/header.php
@@ -1,5 +1,7 @@
 <?php
-
+/*<div class="logout">
+  <button type="button" name="logout-button">Logout</button>
+</div>*/
 
  ?>
 
@@ -8,5 +10,13 @@
     <div class="logo">
       <h3>linkify</h3>
     </div>
+
+    <div class="buttons">
+      <button type="button" name="login-button" class="login-button">Login</button>
+      <button type="button" name="register-button" class="register-button">Sign up</button>
+    </div>
+
   </div>
+
+
 </header>
