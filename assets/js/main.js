684782a0ebe36810c4f167feaad0e84459dc9087
Maria <maria.gnisa@gmail.com>
2016-12-14 11:51:46 +0100

    cookies, in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,27 @@
 'use strict';
 
-  if (!localStorage.getItem('Accepted cookie', 'cookie')) {
-    setCookie();
-  };
+function setCookie(cname, cvalue, cmaxDay) {
+  const d = new Date();
+  d.setTime(d.getTime() + (cmaxDay*24*60*60*1000));
+  const expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires;
+}
 
-  function setCookie(key, value) {
-
-    document.querySelector('.alertbox').style.display = 'block';
-
-    document.querySelector('.button').addEventListener('click', function() {
-         localStorage.setItem('Accepted cookie', 'cookie');
-         document.querySelector('.alertbox').style.display = 'none';
-    });
+function getCookie(cname) {
+  const name = cname + "=";
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0)== ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
+  return "";
+}
+
+function checkCookie() {
+  
+}
