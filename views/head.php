f6ead5c36c2fdc4681b488b1ca6b6227e659cf66
Maria <maria.gnisa@gmail.com>
2016-12-14 10:46:50 +0100

    added head and links

---
+++ views/head.php
@@ -0,0 +1,12 @@
+<?php
+
+ ?>
+
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+<title> <?php echo $title; ?></title>
+
+<link rel="stylesheet" href="/assets/css/reset.css">
+<link rel="stylesheet" href="/assets/css/style.css">
+<script src="assets/js/main.js"> </script>
