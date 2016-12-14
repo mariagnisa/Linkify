dc567e0f448ada865bcccd9ad6c9c1a8e80a08bd
Maria <maria.gnisa@gmail.com>
2016-12-14 18:41:29 +0100

    added show and hide function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 'use strict';
-
+//set cookie
 function setCookie(cname, cvalue, cmaxDay) {
   const d = new Date();
   d.setTime(d.getTime() + (cmaxDay*24*60*60*1000));
   const expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires;
 }
-
+//get cookie
 function getCookie(cname) {
   const name = cname + "=";
   const ca = document.cookie.split(';');
@@ -21,7 +21,36 @@ function getCookie(cname) {
   }
   return "";
 }
-
+//check if there is a cookie
 function checkCookie() {
-  
+  const username = getCookie('uid');
+  document.querySelector('.cookie-box').style.display = 'flex';
+  if (username != "") {
+    getCookie();
+  } else {
+    setCookie();
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const button = document.querySelector('.login-button');
+  const login = document.querySelector('.login-wrapper');
+  button.addEventListener('click', event => {
+    if (login.style.display != 'block') {
+      login.style.display = 'block';
+    } else {
+      login.style.display = 'none';
+    }
+
+  });
+
+  const regButton = document.querySelector('.register-button');
+  const register = document.querySelector('.register-wrapper');
+  regButton.addEventListener('click', event => {
+    if (register.style.display != 'block') {
+      register.style.display = 'block';
+    } else {
+      register.style.display = 'none';
+    }
+  });
+});
