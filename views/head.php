45f04ace1d1247962d93987e761a88ae606cbdf7
Maria <maria.gnisa@gmail.com>
2016-12-18 19:39:36 +0100

    added html, head body tag and header.php

--- views/head.php
+++ views/head.php
@@ -1,13 +1,17 @@
 <?php
 $title = (isset($title)) ? $title:'Linkify';
  ?>
-
+ <html>
+<head>
  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
 <title> <?php echo $title; ?></title>
 
-<link rel="stylesheet" href="assets/css/reset.css">
-<link rel="stylesheet" href="assets/css/style.css">
-<link rel="shortcut icon" href="assets/img/favicon.svg">
-<script src="assets/js/main.js"> </script>
+<link rel="stylesheet" href="/assets/css/reset.css">
+<link rel="stylesheet" href="/assets/css/style.css">
+<link rel="shortcut icon" href="/assets/img/favicon.svg">
+<script src="/assets/js/main.js"> </script>
+</head>
+<body>
+  <?php require_once __DIR__.'/header.php'; ?>
