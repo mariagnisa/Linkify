a773dcc04f84309722c82c428521f33b7ba4bd43
Maria <maria.gnisa@gmail.com>
2016-12-19 13:37:56 +0100

    added posts form

--- views/home.php
+++ views/home.php
@@ -1,6 +1,17 @@
 <?php
+require_once __DIR__.'/../lib/functions.php';
+require_once __DIR__.'/../views/head.php';
  ?>
 
-  <body>
-    <p>Home page</p>
-  </body>
+<h3>A place to rule them all</h3>
+
+<form action="/../lib/posts.php" method="post">
+  <input type="text" name="title" placeholder="Title"><br>
+  <textarea name="description" placeholder="Description"></textarea><br>
+  <input type="url" name="link" placeholder="Your link"><br>
+  <button type="submit" name="postButton">Send</button>
+</form>
+
+
+
+<?php  require_once __DIR__.'/../views/footer.php';  ?>
