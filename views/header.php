f2e5839e766e4af1063f4f2ff9bc0b9b7cb3fd8d
Maria <maria.gnisa@gmail.com>
2016-12-19 13:37:38 +0100

    added links, profile and home

--- views/header.php
+++ views/header.php
@@ -1,27 +1,31 @@
 <?php
-session_start();
+
 $loggedin = isset($_SESSION["loginUser"]);
  ?>
 
 <header>
   <div class="navbar">
-    <div class="logo">
-      <h3>linkify</h3>
-    </div>
 
-      <?php if (!$loggedin): ?>
+    <?php if (!$loggedin): ?>
+      <div class="logo">
+        <h3>linkify</h3>
+      </div>
       <button type="button" name="login-button" class="login-button">Login</button>
       <button type="button" name="register-button" class="register-button">Sign up</button>
     <?php else: ?>
+      <div class="logo">
+        <a href="/views/home.php"><h3>linkify</h3></a>
+      </div>
       <div class="logout-button">
-        <a href="../lib/logout.php">Logout</a>
+        <a href="/../lib/logout.php">Logout</a>
       </div>
       <div class="account-button">
-        <a href="views/settings.php">Account</a>
+        <a href="/views/settings.php">Account</a>
       </div>
 
-      <img class="profile" src="/../assets/img/avatars/avatar<?php echo $_SESSION['loginUser']['uid'] ?>.jpg" alt="">
-
+      <a href="/views/profile.php">
+      <img class="profile" src="/../assets/img/avatars/avatar<?php echo $_SESSION['loginUser']['uid'] ?>.jpg" alt="avatar">
+      </a>
     <?php endif; ?>
   </div>
 
