9ab6f95842b4d8df843847a612f87c830e68b168
Maria <maria.gnisa@gmail.com>
2016-12-19 13:38:17 +0100

    added profile page

---
+++ views/profile.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__.'/../lib/functions.php';
+require_once __DIR__.'/../views/head.php';
+ ?>
+
+<h3>profile</h3>
+
+
+
+
+<?php  require_once __DIR__.'/../views/footer.php';  ?>
