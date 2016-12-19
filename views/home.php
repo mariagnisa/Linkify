b653128f5e249f0f31e1ae227c2c17162b24d3e6
Maria <maria.gnisa@gmail.com>
2016-12-19 22:10:08 +0100

    added print/unsett session message

--- views/home.php
+++ views/home.php
@@ -1,6 +1,16 @@
 <?php
 require_once __DIR__.'/../lib/functions.php';
 require_once __DIR__.'/../views/head.php';
+
+if (isset($_SESSION['error'])) {
+  print_r($_SESSION['error']);
+  unset($_SESSION['error']);
+}
+
+if (isset($_SESSION['message'])) {
+  print_r($_SESSION['message']);
+  unset($_SESSION['message']);
+}
  ?>
 
 <h3>A place to rule them all</h3>
