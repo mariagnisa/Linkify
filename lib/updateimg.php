e086f39a929fc88037e81002006f248ce437af47
Maria <maria.gnisa@gmail.com>
2016-12-19 13:36:05 +0100

    added file max size and validation

--- lib/updateimg.php
+++ lib/updateimg.php
@@ -1,17 +1,30 @@
 <?php
-session_start();
-$uploads = '../assets/img/avatars/';
+  session_start();
+  $uploads = '../assets/img/avatars/';
 
-//if the image folder dont exist, then create one
-if(!file_exists($uploads)) {
-      mkdir($uploads);
+  //if the image folder dont exist, then create one
+  if(!file_exists($uploads)) {
+        mkdir($uploads);
+      }
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $imageData = $_FILES['profileImg'];
+    $tmp = $imageData["tmp_name"];
+
+    //check if image file is fake or not
+    $check = getimagesize($tmp);
+    if ($check == false) {
+      $_SESSION['error'] = 'File is not a image.';
+      header('Location: ../views/settings.php');
+      die();
     }
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $imageData = $_FILES['profileImg'];
-
-  $tmp = $imageData["tmp_name"];
-
+    // Check file size
+    if ($imageData["size"] > 500000) {
+      $_SESSION['error'] = 'Your file is to big, please choose a smaller one.';
+      header('Location: ../views/settings.php');
+      die();
+    }
+    //checks file format (jpg), if upload, not error
    if (exif_imagetype($tmp) === 2) {
      $ext = strrchr($imageData['name'], ".");
      $uid = $_SESSION['loginUser']['uid'];
