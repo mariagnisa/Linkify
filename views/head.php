8dbf334e3b064b6af9897b106a513913cfe3ace0
Maria <maria.gnisa@gmail.com>
2016-12-19 22:01:43 +0100

    new favicon, right format

--- views/head.php
+++ views/head.php
@@ -5,12 +5,13 @@ $title = (isset($title)) ? $title:'Linkify';
 <head>
  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+ 
 
 <title> <?php echo $title; ?></title>
 
 <link rel="stylesheet" href="/assets/css/reset.css">
 <link rel="stylesheet" href="/assets/css/style.css">
-<link rel="icon" href="/assets/img/favicon.svg">
+<link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico">
 <script src="/assets/js/main.js"> </script>
 </head>
 <body>
--- views/home.php
+++ views/home.php
@@ -1,13 +1,22 @@
 <?php
 require_once __DIR__.'/../lib/functions.php';
 require_once __DIR__.'/../views/head.php';
- ?>
+if (isset($_SESSION['error'])) {
+  print_r($_SESSION['error']);
+  unset($_SESSION['error']);
+}
 
+if (isset($_SESSION['message'])) {
+  print_r($_SESSION['message']);
+  unset($_SESSION['message']);
+}
+ ?>
+<h2>home</h2>
 <h3>A place to rule them all</h3>
 
 <form action="/../lib/posts.php" method="post">
   <input type="text" name="title" placeholder="Title"><br>
-  <textarea name="description" placeholder="Description"></textarea><br>
+  <textarea wrap="physical" name="description" placeholder="Description"></textarea><br>
   <input type="url" name="link" placeholder="Your link"><br>
   <button type="submit" name="postButton">Send</button>
 </form>
