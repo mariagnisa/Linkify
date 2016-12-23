6d7f18920f8b1c087a3264a9965e956952c7a927
Maria <maria.gnisa@gmail.com>
2016-12-23 14:32:46 +0100

    fixed profile image to show
    changed session structure

--- views/header.php
+++ views/header.php
@@ -1,20 +1,26 @@
 <?php
+session_start();
+$loggedin = checkUserLogin($db);
 
-$loggedin = isset($_SESSION["loginUser"]);
+if ($loggedin) {
+  $uid = $_SESSION["loginUser"]['uid'];
+  $user = executeGetQuery($db, "SELECT id FROM users WHERE id = '$uid'", true);
+}
  ?>
 
 <header>
   <div class="navbar">
 
-    <?php if (!$loggedin): ?>
+    <?php
+     if (!$loggedin): ?>
       <div class="logo">
-        <h3>linkify</h3>
+        <h2>linkify</h2>
       </div>
       <button type="button" name="login-button" class="login-button">Login</button>
       <button type="button" name="register-button" class="register-button">Sign up</button>
     <?php else: ?>
       <div class="logo">
-        <a href="/views/home.php"><h3>linkify</h3></a>
+        <a href="/views/home.php"><h2>linkify</h2></a>
       </div>
       <div class="logout-button">
         <a href="/../lib/logout.php">Logout</a>
@@ -23,11 +29,18 @@ $loggedin = isset($_SESSION["loginUser"]);
         <a href="/views/settings.php">Account</a>
       </div>
 
-      <a href="/views/profile.php">
-      <img class="profile" src="/../assets/img/avatars/avatar<?php echo $_SESSION['loginUser']['uid'] ?>.jpg" alt="avatar">
-      </a>
+      <?php if (!hasImage($_SERVER['DOCUMENT_ROOT']."/assets/img/avatars")): ?>
+              <a href="/views/profile.php">
+                <div class="profile">
+                  <p class="profile-text">Profile</p>
+                </div>
+              </a>
+        <?php else: ?>
+              <a href="/views/profile.php">
+              <img class="profile" src="../assets/img/avatars/avatar<?php echo $user['id'] ?>.jpg" alt="avatar">
+              </a>
+      <?php endif; ?>
     <?php endif; ?>
   </div>
-
 
 </header>
