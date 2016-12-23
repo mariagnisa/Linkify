058994e585b1883b004a4cf5812e2663d6a1f827
Maria <maria.gnisa@gmail.com>
2016-12-23 12:17:35 +0100

    added meta tags, no cache

--- views/head.php
+++ views/head.php
@@ -5,6 +5,11 @@ $title = (isset($title)) ? $title:'Linkify';
 <head>
  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+ <meta http-equiv="cache-control" content="max-age=0" />
+ <meta http-equiv="cache-control" content="no-cache" />
+ <meta http-equiv="expires" content="0" />
+ <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+ <meta http-equiv="pragma" content="no-cache" />
 
 <title> <?php echo $title; ?></title>
 
