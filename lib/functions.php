77f13f6db13e5e07731c037b8e114c2fed0508fb
Maria <maria.gnisa@gmail.com>
2017-01-11 15:35:33 +0100

    added verification for two passwords at registration

--- lib/functions.php
+++ lib/functions.php
@@ -15,11 +15,11 @@ function validateEmail($db, $email) {
   $validEmail = true;
 
   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $validEmail = false;
+    $validEmail = false;
   } else {
-      if (executeGetQuery($db, "SELECT id FROM users WHERE email = '$email'", true)) {
-          $validEmail = false;
-      }
+    if (executeGetQuery($db, "SELECT id FROM users WHERE email = '$email'", true)) {
+      $validEmail = false;
+    }
   }
   return $validEmail;
 }
@@ -37,42 +37,49 @@ function validateUsername($db, $username) {
 function escapeData($db, $items) {
 
   foreach ($items as $i => $item) {
-        $items[$i] = mysqli_real_escape_string($db, $item);
-    }
-    return $items;
+    $items[$i] = mysqli_real_escape_string($db, $item);
+  }
+  return $items;
 }
 
 //Insert new user to the database and validates username and email against the db
-function registerNewUser($db, $name, $username, $email, $password) {
+function registerNewUser($db, $name, $username, $email, $password, $repeatPass) {
   // Escape all the data received from the user
-    list($name, $username, $email, $password) = escapeData($db, [$name, $username, $email, $password]);
+  list($name, $username, $email, $password, $repeatPass) = escapeData($db, [$name, $username, $email, $password, $repeatPass]);
 
-    //validate email
-    if (!validateEmail($db, $email)) {
-      session_start();
-      $_SESSION['error'] = 'The email you provided was invalid or already in use';
-      return false;
-    }
-
-    //validate username
-    if (!validateUsername($db, $username)) {
-      session_start();
-      $_SESSION['error'] = 'The username you provided is already in use.';
-      return false;
-    }
-
-    // Hash password
-    $password = password_hash($password, PASSWORD_BCRYPT);
-
-    //check if registration succeded
+  //validate email
+  if (!validateEmail($db, $email)) {
     session_start();
-    if (!executePosts($db, "INSERT INTO users (email, name, password, username) VALUES ('$email', '$name', '$password', '$username')")) {
-      $_SESSION['error'] = 'Failed to registrate.';
-      return false;
-    } else {
-      $_SESSION['message'] = 'You are now registrated. Please login in to continue.';
-      return true;
-    }
+    $_SESSION['error'] = 'The email you provided was invalid or already in use';
+    return false;
+  }
+
+  //validate username
+  if (!validateUsername($db, $username)) {
+    session_start();
+    $_SESSION['error'] = 'The username you provided is already in use.';
+    return false;
+  }
+
+  //checks if the passwords matches, if not error
+  if (!($password === $repeatPass)) {
+    $_SESSION['error'] = 'Passwords do not match. Please try again.';
+    header('Location: /');
+    die();
+  }
+
+  // Hash password
+  $password = password_hash($password, PASSWORD_BCRYPT);
+
+  //check if registration succeded
+  session_start();
+  if (!executePosts($db, "INSERT INTO users (email, name, password, username) VALUES ('$email', '$name', '$password', '$username')")) {
+    $_SESSION['error'] = 'Failed to registrate.';
+    return false;
+  } else {
+    $_SESSION['message'] = 'You are now registrated. Please login in to continue.';
+    return true;
+  }
 }
 
 //validates a user against db and if correct, the user logs into linkify
@@ -119,4 +126,4 @@ function hasImage($imageFolder) {
   }
   return false;
 }
- ?>
+?>
