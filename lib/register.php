2afc0ac5b1a05b9325e746a958998584cbe4d929
Maria <maria.gnisa@gmail.com>
2017-01-11 15:35:45 +0100

    added repeat password

--- lib/register.php
+++ lib/register.php
@@ -4,8 +4,8 @@ require_once __DIR__.'/functions.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   //check if all fileds are filled in
-  if ($_POST['fullname'] !== "" && $_POST['username'] !== "" && $_POST['email'] !== "" && $_POST['password'] !== "") {
-    registerNewUser($db, $_POST['fullname'], $_POST['username'], $_POST['email'], $_POST['password']);
+  if ($_POST['fullname'] !== "" && $_POST['username'] !== "" && $_POST['email'] !== "" && $_POST['password'] !== "" && $_POST['repeatPass'] !== "") {
+    registerNewUser($db, $_POST['fullname'], $_POST['username'], $_POST['email'], $_POST['password'], $_POST['repeatPass']);
   } else {
     session_start();
     echo 'missing fileds';
@@ -13,6 +13,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   header('Location: /');
   die();
 }
-
 
  ?>
