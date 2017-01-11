34e49180a0589c389aa8b0f731e2a72501d34dbe
Maria <maria.gnisa@gmail.com>
2017-01-11 21:49:46 +0100

    added edit post form, and edit button if logged in

--- views/home.php
+++ views/home.php
@@ -16,37 +16,53 @@ if (isset($_SESSION['message'])) {
 $uid = $_SESSION['loginUser']['uid'];
 
 $posts = executeGetQuery($db, "SELECT * FROM posts");
- ?>
+
+?>
 
 <h3>A place to rule them all</h3>
-  <div class="post">
-    <form action="/../lib/posts.php" method="post">
-      <input type="text" name="title" placeholder="Title"><br>
-      <textarea name="description" placeholder="Description"></textarea><br>
-      <input type="url" name="link" placeholder="Your link"><br>
-      <button type="submit" name="postButton">Share</button>
-    </form>
-  </div>
+<div class="post">
+  <form action="/../lib/posts.php" method="post">
+    <input type="text" name="title" placeholder="Title"><br>
+    <textarea name="description" placeholder="Description"></textarea><br>
+    <input type="url" name="link" placeholder="Your link"><br>
+    <button type="submit" name="postButton">Share</button>
+  </form>
+</div>
+
 <?php
 foreach ($posts as $post):
   $date = $post['published'];
   $date= date("l jS \of F Y");?>
-    <div class="posts">
-      <?php if ($post['uid'] === $uid): ?>
-              <img class="posts-edit" src="../assets/img/edit.png" alt="Edit">
-      <?php endif; ?>
-      <img class="posts-avatar" src="../assets/img/avatars/avatar<?php echo $post['uid'] ?>.jpg" alt="avatar">
-      <a href="<?php echo $post['link']; ?>">
+  <div class="posts">
+    <?php if ($post['uid'] === $uid): ?>
+      <img class="posts-edit" src="../assets/img/edit.png" alt="Edit">
+    <?php endif; ?>
+    <img class="posts-avatar" src="../assets/img/avatars/avatar<?php echo $post['uid'] ?>.jpg" alt="avatar">
+    <a href="<?php echo $post['link']; ?>">
       <div class="post-title"><?php echo $post['title']; ?></div>
-      </a>
-      <div class="post-content"><?php echo $post['content']; ?></div>
-      <div class="post-published"><?php echo 'Published ' . $date; ?></div>
-      <div class="comment-button">
-        <a href="../views/viewPost.php?post=<?php echo $post['id'] ?>">Comments</a>
-      </div>
+    </a>
+    <div class="post-content"><?php echo $post['content']; ?></div>
+    <div class="post-published"><?php echo 'Published ' . $date; ?></div>
+    <div class="comment-button">
+      <a href="../views/viewPost.php?post=<?php echo $post['id'] ?>">Comments</a>
     </div>
-<?php
-  endforeach;
- ?>
+  </div>
+
+  <div class="edit-post">
+    <form action="../lib/updatePosts.php" method="post">
+      <input type="hidden" name="postId" value="<?php echo $post['id'] ?>">
+      <input type="text" name="editTitle" value="<?php echo $post['title']; ?>">
+      <textarea name="editContent"><?php echo $post['content']; ?></textarea>
+      <input type="url" name="editLink" value="<?php echo $post['link']; ?>">
+      <button class="edit-post-save" type="submit" name="editButton">Save changes</button>
+      <button class="edit-post-delete" type="submit" name="deleteButton">Delete post</button>
+    </form>
+  </div>
+  <?php
+endforeach;
+?>
+
+
+
 
 <?php  require_once __DIR__.'/../views/footer.php';  ?>
