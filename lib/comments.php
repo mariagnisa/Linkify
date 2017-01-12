575f8f0111903f38e18ce48abf39074198a39fc5
Maria <maria.gnisa@gmail.com>
2017-01-12 21:57:14 +0100

    changed header location, deleted session message/error

--- lib/comments.php
+++ lib/comments.php
@@ -4,9 +4,6 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   require_once __DIR__.'/functions.php';
 
-  unset($_SESSION['message']);
-  unset($_SESSION['error']);
-
   //check if the user is logged in
   if (!checkUserLogin($db)) {
     header('Location: /');
@@ -27,18 +24,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 
   //escape data
-  $comment = mysqli_real_escape_string($db, $_POST['comment']);
+  $comment = mysqli_real_escape_string($db, $comment);
 
   //if everything is good, put into db, else error
   if (!executePosts($db, "INSERT INTO comments (comment, published, uid, posts_id) VALUES ('$comment', '$date', '$user', '$postId')")) {
     $_SESSION['error'] = 'Something went wrong with the database request.';
-    header('Location: ../views/viewPost.php');
+    header("Location: ../views/viewPost.php?post=$postId");
     die();
   } else {
     $_SESSION['message'] = 'Your comment has been uploaded.';
   }
 
-  header('Location: ../views/viewPost.php');
+  header("Location: ../views/viewPost.php?post=$postId");
   die();
 }
 
