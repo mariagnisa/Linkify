7a09940c8979b8bc18e7ce4b15ccf609922c06b0
Maria <maria.gnisa@gmail.com>
2017-01-13 18:56:12 +0100

    changed structure, and comment

--- views/profile.php
+++ views/profile.php
@@ -5,46 +5,48 @@ require_once __DIR__.'/../views/head.php';
 
 $uid = $_SESSION['loginUser']['uid'];
 
+//Fetch data from the logged in user
 $user = executeGetQuery($db, "SELECT * FROM users WHERE id = '$uid'", true);
+//Fetch all posts from the logged in user
 $posts = executeGetQuery($db, "SELECT * FROM posts WHERE uid = '$uid'");
 
- ?>
+?>
 
+<div class="profile-intro">
+  <h3>Profile info</h3>
+</div>
 <div class="profile-info">
-  <h3>Profile</h3>
-  <div class="profile-username">
-    <?php echo $user['username']; ?>
-  </div>
-  <div class="profile-name">
-    <?php echo $user['name']; ?>
-  </div>
-  <div class="profile-bio">
-    <?php echo $user['bio']; ?>
-  </div>
+  <p>Username: <?php echo $user['username']; ?> </p>
+  <p>Fullname: <?php echo $user['name']; ?> </p>
+  <p>Bio: <?php echo $user['bio']; ?> </p>
+  <p style="font-style: italic">If you want to change your settings please visit your account page to update them.</p>
 </div>
 <br>
-  <?php
-
-  foreach ($posts as $post):
-    $date = strtotime($post['published']);
-    $date = date("l jS \of F Y", $date);
-    if ($post['uid'] === $uid && sizeof($post) != 0):?>
-      <div class="profile-posts">
-        <a href="<?php echo $post['link']; ?>">
-        <div class="post-title"><?php echo $post['title']; ?></div>
-        </a>
-        <div class="post-content"><?php echo $post['content']; ?></div>
-        <div class="post-published"><?php echo 'Published ' . $date; ?></div>
-      </div>
-  <?php endif;
-    endforeach;
-
-      if (sizeof($posts) == 0): ?>
-        <div class="profile-empty-posts">
-          <p>No posts yet! What are you waiting for?</p>
-        </div>
+<div class="profile-posts-intro">
+  <h3>Your posts</h3>
+</div>
+<?php
+foreach ($posts as $post):
+  $date = strtotime($post['published']);
+  $date = date("l jS \of F Y", $date);
+  //Checks if the user have any posts, if there are some print all of them
+  if ($post['uid'] === $uid && sizeof($post) != 0):?>
+  <div class="profile-posts">
+    <a href="<?php echo $post['link']; ?>">
+      <div class="profile-post-title"><?php echo $post['title']; ?></div>
+    </a>
+    <div class="profile-post-content"><?php echo $post['content']; ?></div>
+    <div class="profile-post-published"><?php echo 'Published ' . $date; ?></div>
+  </div>
+<?php endif;
+endforeach;
+//If the user have not made any posts, show message
+if (sizeof($posts) == 0): ?>
+<div class="profile-empty-posts">
+  <p>No posts yet! What are you waiting for?</p>
+</div>
 <?php die();
-        endif;
+endif;
 ?>
 
 <?php  require_once __DIR__.'/../views/footer.php';  ?>
