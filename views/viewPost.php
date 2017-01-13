99237bbfd513f4e60b9f6e294451cb571ce7b157
Maria <maria.gnisa@gmail.com>
2017-01-13 18:53:29 +0100

    fixed and structured my css

--- views/viewPost.php
+++ views/viewPost.php
@@ -3,53 +3,63 @@
 require_once __DIR__.'/../lib/functions.php';
 require_once __DIR__.'/../views/head.php';
 
+//Checks if the post id is set, if not the user will be thrown back to previous page
 if (!isset($_GET['post'])) {
   header('Location:../views/home.php');
   die();
 }
 
+//Get the right post id from comment link
 $postId = $_GET['post'];
+//Fetch the specifik post
 $post = executeGetQuery($db, "SELECT * FROM posts WHERE id = '$postId'", true);
+//Fetch all comments to the specific post
 $comments = executeGetQuery($db, "SELECT * FROM comments WHERE posts_id = '$postId'");
 
 $date = strtotime($post['published']);
 $date = date("l jS \of F Y", $date);?>
-<div class="posts">
-  <img class="posts-avatar" src="../assets/img/avatars/avatar<?php echo $post['uid'] ?>.jpg" alt="avatar">
+<div class="view-posts">
+  <img class="view-posts-avatar" src="../assets/img/avatars/avatar<?php echo $post['uid']; ?>.jpg" alt="avatar">
   <a href="<?php echo $post['link']; ?>">
-    <div class="post-title"><?php echo $post['title']; ?></div>
+    <div class="view-post-title"><?php echo $post['title']; ?></div>
   </a>
-  <div class="post-content"><?php echo $post['content']; ?></div>
-  <div class="post-published"><?php echo 'Published ' . $date; ?></div>
+  <div class="view-post-content"><?php echo $post['content']; ?></div>
+  <div class="view-post-published"><?php echo 'Published ' . $date; ?></div>
 </div>
 
+<!-- Share comment form -->
 <div class="comment-form">
   <form action="../lib/comments.php" method="post">
-    <input type="hidden" name="postId" value="<?php echo $post['id']?>">
+    <!-- Sending the specific post id -->
+    <input type="hidden" name="postId" value="<?php echo $post['id']; ?>">
     <input type="text" name="comment" placeholder="Write comment"><br>
     <button type="submit" name="comment-form-button">Comment</button>
   </form>
 </div>
 
-<h3>Comments</h3>
+<div class="comment-intro">
+  <h3>Comments</h3>
+</div>
 
 <?php
+//Prints all comments
 foreach ($comments as $comment):
-    $date = strtotime($comment['published']);
-    $date = date("l jS \of F Y", $date);?>
-    <div class="post-comments">
-      <img src="../assets/img/avatars/avatar<?php echo $comment['uid'] ?>.jpg" alt="avatar">
-      <div class="comment"><?php echo $comment['comment']; ?></div>
-      <div class="comment-published"><?php echo 'Published ' . $date; ?></div>
-    </div>
-    <?php
+  $date = strtotime($comment['published']);
+  $date = date("l jS \of F Y", $date);?>
+  <div class="post-comments">
+    <img src="../assets/img/avatars/avatar<?php echo $comment['uid']; ?>.jpg" alt="avatar">
+    <div class="comment"><?php echo $comment['comment']; ?></div>
+    <div class="comment-published"><?php echo 'Published ' . $date; ?></div>
+  </div>
+  <?php
 endforeach;
 
+//If there is no comments
 if (sizeof($comments) == 0): ?>
- <div class="post-comment-empty">
-   <p>No comments yet! Be the first one to write what you think!</p>
- </div>
- <?php die();
+<div class="post-comment-empty">
+  <p>No comments yet! Be the first one to write what you think!</p>
+</div>
+<?php die();
 endif;
 
 ?>
