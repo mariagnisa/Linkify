11ece5fe89dfdc242a4e9233d34d288177493f88
Maria <maria.gnisa@gmail.com>
2017-01-15 17:24:41 +0100

    fixed indent and comments

--- lib/login.php
+++ lib/login.php
@@ -9,15 +9,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       session_start();
       //store the logged in users uid
       $_SESSION['loginUser'] = [
-      'uid' => $uid
+        'uid' => $uid
       ];
       setcookie('username', $_POST['username'], time() + (86400 * 30), "/");
       setcookie('password', $_POST['password'], time() + (86400 * 30), "/");
     }
   } else {
-    //if missing fields
+    //if missing fields throw an error
     session_start();
-    $_SESSION['error'] = 'Please fill out all fields.';
+    $_SESSION['error'] = 'Please fill in all fields.';
+    header('Location: /');
+    die();
   }
 
   header('Location: /');
@@ -25,4 +27,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 
- ?>
+?>
