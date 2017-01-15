06fc3ff34b8c30e3c9ff4744c32c55ba391b9a20
Maria <maria.gnisa@gmail.com>
2017-01-15 17:23:51 +0100

    fixed indent

--- index.php
+++ index.php
@@ -1,28 +1,21 @@
 <?php
 
-  require_once __DIR__.'/lib/functions.php';
-  require_once __DIR__.'/views/head.php';
+require_once __DIR__.'/lib/functions.php';
+require_once __DIR__.'/views/head.php';
 
-  $loggedInUser = checkUserLogin($db);
+$loggedInUser = checkUserLogin($db);
 
-  $title = 'Linkify';
+$title = 'Linkify';
 
-    //checks if a user has a active session
-    if ($loggedInUser) {
-      //if active session, the user will come to home page
-      require_once __DIR__.'/views/home.php';
-    } else {
-      //otherwise user will come to authenication page for login/register
-      require_once __DIR__.'/views/authenication.php';
-    }
+//checks if a user has a active session
+if ($loggedInUser) {
+  //if active session, the user will come to home page
+  require_once __DIR__.'/views/home.php';
+} else {
+  //otherwise user will come to authenication page for login/register
+  require_once __DIR__.'/views/authenication.php';
+}
 
-   ?>
-  
-
-   <div class="cookie-box">
-    <p>This website uses <a href="https://www.cookielaw.org/the-cookie-law">cookies</a> to ensure you get the best experience on our website.</p>
-  <button class="cookie-box-button" type="button" name="button">Accept</button>
-  </div>
-
+?>
 
 <?php require_once __DIR__.'/views/footer.php'; ?>
