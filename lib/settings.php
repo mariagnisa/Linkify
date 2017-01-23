ed62dcf2b8fab0c63a7156a1e1b2cc33692e6026
Maria <maria.gnisa@gmail.com>
2017-01-23 21:55:45 +0100

    fixed bugg

--- lib/settings.php
+++ lib/settings.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   require_once __DIR__.'/functions.php';
@@ -10,39 +9,86 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     die();
   }
 
-  $save = $_POST['saveButton'];
   //check if the save button is clicked
-  if ($save) {
-    //check if email and password inputs are empty, if they are throw an error
-    if (empty($_POST['changeEmail']) || empty($_POST["confirmPassword"])) {
-      $_SESSION['error'] = 'Please fill in all fields.';
+  if ($_POST['saveButton']) {
+
+    //check if password is empty, if it is throw an error
+    if (empty($_POST['confirmPassword'])) {
+      $_SESSION['error'] = 'Please fill in password to save.';
       header('Location: /account');
       die();
     }
 
-    //validate email, if invalid throw an error
-    if (!filter_var($_POST['changeEmail'], FILTER_VALIDATE_EMAIL)) {
-      $_SESSION['error'] = 'The email you provided was invalid';
+    //check if inputs are empty, if they are throw an error
+    if (empty($_POST['changeEmail']) && empty($_POST['changeUsername']) && empty($_POST['newPassword']) && empty($_POST['repeatPassword']) && empty($_POST['changeText'])) {
+      $_SESSION['error'] = 'Please fill in some fields to change.';
       header('Location: /account');
       die();
     }
 
     //validate password, checks which user through session. If wrong password, throw an error
     if (!validateUserPassword($db, $_SESSION['loginUser']['uid'], $_POST['confirmPassword'])) {
-      $_SESSION['error'] = 'Invalid password.';
+      $_SESSION['error'] = 'Invalid password. Please try again';
       header('Location: /account');
       die();
     }
 
     $uid = $_SESSION['loginUser']['uid'];
-    //Escape data for security
-    $email = mysqli_real_escape_string($db, $_POST['changeEmail']);
-    $bio = mysqli_real_escape_string($db, $_POST['changeText']);
 
-    //the sql beginning for update email and bio, password further below
-    $updateSql = "UPDATE users SET email = '$email', bio = '$bio'";
+    //checks if email input is not empty
+    if (!empty($_POST['changeEmail'])) {
+      //Escape data for security
+      $email = mysqli_real_escape_string($db, $_POST['changeEmail']);
 
-    //if new password is not empty, checks if repeat password is filled in
+      //validate email, if invalid throw an error
+      if (!validateEmail($db, $email)) {
+        $_SESSION['error'] = 'The email you provided was invalid or already in use';
+        header('Location: /account');
+        die();
+      }
+
+      //Update the email
+      if (!updateUser($db, $uid, $email, 'email')) {
+        $_SESSION['error'] = 'Something went wrong with the request';
+        header('Location: /account');
+        die();
+      }
+    }
+
+    //checks if username input is not empty
+    if (!empty($_POST['changeUsername'])) {
+      //Escape data for security
+      $username = mysqli_real_escape_string($db, $_POST['changeUsername']);
+
+      //validate username, if already in use throw an error
+      if (!validateUsername($db, $username)) {
+        $_SESSION['error'] = 'The username you provided is already in use';
+        header('Location: /account');
+        die();
+      }
+
+      //Update the username
+      if (!updateUser($db, $uid, $username, 'username')) {
+        $_SESSION['error'] = 'Something went wrong with the request';
+        header('Location: /account');
+        die();
+      }
+    }
+
+    //checks if bio input is not empty
+    if (!empty($_POST['changeText'])) {
+      //Escape data for security
+      $bio = mysqli_real_escape_string($db, $_POST['changeText']);
+
+      //Update bio
+      if (!updateUser($db, $uid, $bio, 'bio')) {
+        $_SESSION['error'] = 'Something went wrong with the request';
+        header('Location: /account');
+        die();
+      }
+    }
+
+    //If the new password input is not empty
     if(!empty($_POST['newPassword'])) {
       //If new password is filled in and not the repeat password, throw an error
       if (empty($_POST['repeatPassword'])) {
@@ -59,20 +105,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header('Location: /account');
         die();
       }
+      //Escape password
+      $newPassword = mysqli_real_escape_string($db, $newPassword);
+
       //hash the new password
       $password = password_hash($newPassword, PASSWORD_BCRYPT);
-      //adding password to the sql update
-      $updateSql = $updateSql . ", password = '$password'";
-    }
-    //the end for the sql update statement,
-    $whereClause =  " WHERE id = '$uid'";
 
-    //change email and/or password, if not throw an error
-    if (!executePosts($db, $updateSql.$whereClause)) {
-      $_SESSION['error'] = 'Something went wrong with the database request.';
-    } else {
-      $_SESSION['message'] = 'Your settings has been updated';
+      //Update the password
+      if (!updateUser($db, $uid, $password, 'password')) {
+        $_SESSION['error'] = 'Something went wrong with the request';
+        header('Location: /account');
+        die();
+      }
     }
+
+    $_SESSION['message'] = 'Your settings has been updated';
     header('Location: /account');
     die();
   }
